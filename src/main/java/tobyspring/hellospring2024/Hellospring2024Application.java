4e543ced331b29a1368ae0d9ddce88dfca282ef4
jinhyeong <jinhyeong.b@gmail.com>
2024-08-15 17:38:40 +0900

    init commit

---
+++ src/main/java/tobyspring/hellospring2024/Hellospring2024Application.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellospring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellospring2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/tobyspring/hellospring2024/Hellospring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hellospring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
