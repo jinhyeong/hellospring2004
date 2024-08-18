ee43ef8a6079c9560e037d051c16d184a916d42b
jinhyeong <jinhyeong.b@gmail.com>
2024-08-18 17:28:29 +0900

    클래스의 분리 - 생성자에서 WebApiExRateProvider 초기화

--- src/main/java/tobyspring/hellospring2024/PaymentService.java
+++ src/main/java/tobyspring/hellospring2024/PaymentService.java
@@ -5,12 +5,18 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
+
+	private WebApiExRateProvider exRateProvider;
+
+	public PaymentService() {
+		exRateProvider = new WebApiExRateProvider();
+	}
+
 	public Payment prepare(
 			Long orderId,
 			String currency,
 			BigDecimal foreignCurrentAmount
 	) throws IOException {
-		WebApiExRateProvider exRateProvider = new WebApiExRateProvider();
 		BigDecimal exRate = exRateProvider.getWebExRate(currency);
 
 		BigDecimal convertedAmount = foreignCurrentAmount.multiply(exRate);
