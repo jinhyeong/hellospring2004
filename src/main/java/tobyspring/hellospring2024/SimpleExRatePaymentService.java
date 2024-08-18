360b2d4c8837c92cecc43f99cc87eabac1bdc462
jinhyeong <jinhyeong.b@gmail.com>
2024-08-18 16:36:39 +0900

    상속을 통한 확장 - SimpleExRatePaymentService 구현

---
+++ src/main/java/tobyspring/hellospring2024/SimpleExRatePaymentService.java
@@ -0,0 +1,14 @@
+package tobyspring.hellospring2024;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class SimpleExRatePaymentService extends PaymentService {
+	@Override
+	BigDecimal getExRate(String currency) throws IOException {
+		if (currency.equals("USD")) {
+			return new BigDecimal("1000");
+		}
+		throw new IllegalArgumentException("Unsupported currency: " + currency);
+	}
+}
